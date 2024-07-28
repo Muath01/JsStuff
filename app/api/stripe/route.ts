fffa9fcdd9581734a2e1a41813871c44552ae948
muath <muath.khalifa@yahoo.com>
2024-07-28 09:40:53 +0100

    added an order email, after payment succesfull

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,55 @@
+// this emails the product to user after they purchase
+
+import ProductEmail from "@/app/components/ProductEmail";
+import prisma from "@/app/lib/db";
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
+
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(req: Request) {
+  const body = await req.text();
+
+  const signature = headers().get("Stripe-Signature") as string;
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(
+      body,
+      signature,
+      process.env.STRIPE_SECRET_WEBHOOK as string
+    );
+  } catch (error: unknown) {
+    return new Response("Webhook error", { status: 400 });
+  }
+
+  switch (event.type) {
+    case "checkout.session.completed": {
+      const session = event.data.object;
+
+      const link = session.metadata?.link;
+      const { data, error } = await resend.emails.send({
+        from: "ReserverIt <onboarding@resend.dev>",
+        to: ["muath.khalifa@yahoo.com"],
+        subject: "Your Product from ReserverIt",
+        react: ProductEmail({ link: link as string }),
+      });
+      console.log("link: ", link);
+      console.log("data: ", data);
+      console.log("error: ", error);
+      if (error) {
+        console.log("error: ", error);
+        return console.error({ error });
+      }
+      break;
+    }
+    default: {
+      console.log("unhandled event");
+    }
+  }
+
+  return new Response(null, { status: 200 });
+}
---
+++ app/components/ProductEmail.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import {
+  Html,
+  Head,
+  Preview,
+  Tailwind,
+  Body,
+  Text,
+  Section,
+  Button,
+  Container,
+} from "@react-email/components";
+function ProductEmail({ link }: { link: string }) {
+  return (
+    <Html>
+      <Head />
+      <Preview>Your Product is here...</Preview>
+      <Tailwind>
+        <Body className="bg-white font-sans border-2">
+          <Container style={container}>
+            <Text className="text-2xl font-semibold">Hi Friend</Text>
+            <Text className="text-sm text-gray-600">
+              Thank you for your business
+            </Text>
+            <Section className="w-full flex justify-center mt-7">
+              <Button
+                href={link}
+                className="text-white bg-red-500 rounded-lg px-10 py-4 cursor-pointer"
+              >
+                Click to Download
+              </Button>
+            </Section>
+            <Text className="text-lg">
+              Best, <br /> ReserverIt Team
+            </Text>
+          </Container>
+        </Body>
+      </Tailwind>
+    </Html>
+  );
+}
+
+const container = {
+  margin: "0 auto",
+  padding: "20px 0 48px",
+};
+
+export default ProductEmail;
